4415741a06487b803b63d300b747657c55f856da
SumeetRohra <sumeetrohra1@gmail.com>
2018-04-26 22:59:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './Components/Common';
+import LoginForm from './Components/LoginForm';
+
+
+class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp(
+            {
+                apiKey: 'AIzaSyA_Ua_Qdc97bARPBPGMzjpLL_tMboFcEtM',
+                authDomain: 'auth-3a578.firebaseapp.com',
+                databaseURL: 'https://auth-3a578.firebaseio.com',
+                projectId: 'auth-3a578',
+                storageBucket: 'auth-3a578.appspot.com',
+                messagingSenderId: '889472800763'
+              }
+        );
+    }
+
+    render() {
+        return (
+            <View>
+                <Header headerText="Auth!" />
+                <LoginForm />
+            </View>
+        );
+    }
+}
+
+export default App;
---
+++ src/Components/LoginForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+import { Button, Card, CardSection } from './Common';
+
+class LoginForm extends Component {
+    state = { text: '' };
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <TextInput
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text })}
+                        style={{ height: 40, width: 100 }}
+
+                    />
+                </CardSection>
+                <CardSection />
+                <CardSection>
+                    <Button title='Login' />
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+export default LoginForm;
